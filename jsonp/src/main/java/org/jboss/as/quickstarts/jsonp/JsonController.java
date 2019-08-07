8f2d7a00333d0005992335d997cf61dd6e261f5e
penehyba <penehyba@redhat.com>
2019-08-07 14:45:41 +0200

    JSONP 1.1 pointer patch demo app

--- jsonp/src/main/java/org/jboss/as/quickstarts/jsonp/JsonController.java
+++ jsonp/src/main/java/org/jboss/as/quickstarts/jsonp/JsonController.java
@@ -17,13 +17,20 @@
 package org.jboss.as.quickstarts.jsonp;
 
 import java.io.StringReader;
+import java.util.stream.IntStream;
 
 import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonException;
+import javax.json.JsonNumber;
+import javax.json.JsonPatch;
+import javax.json.JsonPointer;
+import javax.json.JsonStructure;
+import javax.json.stream.JsonCollectors;
 import javax.json.stream.JsonParser;
 import javax.json.stream.JsonParser.Event;
 
@@ -37,22 +44,189 @@ public class JsonController {
 
     private String parsedResult;
 
+    private String pointerResult = "empty";
+
+    private String query = "/name";
+
+    private final String defaultJsonString = "{\"name\":\"Rafael Benevides\",\"age\":35,\"enabled\":true,\"phones\":[\"919-555-1324\",\"919-555-0987\"],\"address\":{\"street\":\"4811 Benevides Street\",\"apt\":456,\"city\":\"Raleigh, NC\",\"zip\":\"27123\"}}";
+
+    private final String mergeJsonString = "{\"name\":\"Rafael\",\"age\":35,\"enabled\":false,\"phones\": null,\"address\":{\"street\":\"4811 Benevides Street\",\"apt\":456,\"city\":\"Raleigh, NC\",\"zip\":\"27123\"}}";
+
+    private final String mergeMinimalJsonString = "{\"name\":\"Rafael\",\"enabled\":false,\"phones\":null}";
+
+    private final String expectedMergeResult = "{\"name\":\"Rafael\",\"age\":35,\"enabled\":false,\"address\":{\"street\":\"4811 Benevides Street\",\"apt\":456,\"city\":\"Raleigh, NC\",\"zip\":\"27123\"}}";
+
+    private final String expectedJsonCollectorResult = "[{\"key1\":\"value1\"},{\"key2\":\"value2\"},{\"key3\":\"value3\"},{\"key4\":\"value4\"},{\"key5\":\"value5\"}]";
+
     public void generateJson() {
         jsonString = Json.createObjectBuilder()
-            .add("name", person.getName())
-            .add("age", person.getAge())
-            .add("enabled", person.getEnabled())
-            .add("phones", Json.createArrayBuilder()
-                .add(person.getPhone1())
-                .add(person.getPhone2())
-            )
-            .add("addrees", Json.createObjectBuilder()
-                .add("street", person.getAddressStreet())
-                .add("apt", person.getAddressApt())
-                .add("city", person.getAddressCity())
-                .add("zip", person.getAddressZip())
-            )
-            .build().toString();
+                .add("name", person.getName())
+                .add("age", person.getAge())
+                .add("enabled", person.getEnabled())
+                .add("phones", Json.createArrayBuilder()
+                        .add(person.getPhone1())
+                        .add(person.getPhone2())
+                )
+                .add("address", Json.createObjectBuilder()
+                        .add("street", person.getAddressStreet())
+                        .add("apt", person.getAddressApt())
+                        .add("city", person.getAddressCity())
+                        .add("zip", person.getAddressZip())
+                )
+                .build().toString();
+    }
+
+
+    public void pointerContainsValue() {
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            final JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            JsonPointer pointer = Json.createPointer(query);
+            pointerResult = String.valueOf(pointer.containsValue(jsonStructure));
+        }
+    }
+
+    public void pointerGetValue() {
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            final JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            JsonPointer pointer = Json.createPointer(query);
+            pointerResult = String.valueOf(pointer.getValue(jsonStructure));
+        }
+    }
+
+    public void pointerAdd() {
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            JsonPointer jsonPointer = Json.createPointer("/children");
+            JsonNumber jsonNumber = Json.createValue(3);
+            jsonStructure = jsonPointer.add(jsonStructure, jsonNumber);
+            jsonString = String.valueOf(jsonStructure);
+        }
+    }
+
+    public void pointerReplace() {
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            JsonPointer jsonPointer = Json.createPointer("/children");
+            JsonNumber jsonNumberNewValue = Json.createValue(5);
+            try {
+                jsonStructure = jsonPointer.replace(jsonStructure, jsonNumberNewValue);
+                jsonString = String.valueOf(jsonStructure);
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void pointerRemove() {
+        JsonPointer jsonPointer = Json.createPointer("/children");
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            try {
+                jsonStructure = jsonPointer.remove(jsonStructure);
+                jsonString = String.valueOf(jsonStructure);
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void patchAdd() {
+        JsonPatch jsonPatch = Json.createPatchBuilder().add("/friends", 12).build();
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            try {
+                jsonString = String.valueOf(jsonPatch.apply(jsonStructure));
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void patchRemove() {
+        JsonPatch jsonPatch = Json.createPatchBuilder().remove("/friends").build();
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            try {
+                jsonString = String.valueOf(jsonPatch.apply(jsonStructure));
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void patchMove() {
+        JsonPatch jsonPatch = Json.createPatchBuilder().move("/bestfriends", "/friends").build();
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            try {
+                jsonString = String.valueOf(jsonPatch.apply(jsonStructure));
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void patchCopy() {
+        JsonPatch jsonPatch = Json.createPatchBuilder().copy("/friends", "/age").build();
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            try {
+                jsonString = String.valueOf(jsonPatch.apply(jsonStructure));
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void patchReplace() {
+        JsonPatch jsonPatch = Json.createPatchBuilder().replace("/friends", 14).build();
+        try (StringReader stringReader = new StringReader(jsonString)) {
+            JsonStructure jsonStructure = Json.createReader(stringReader).read();
+            try {
+                jsonString = String.valueOf(jsonPatch.apply(jsonStructure));
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void mergePatch(boolean minimal) {
+        String mergeString = minimal ? this.mergeMinimalJsonString : this.mergeJsonString;
+        try (StringReader readerDefault = new StringReader(defaultJsonString);
+             StringReader readerMerge = new StringReader(mergeString);
+        ) {
+            JsonStructure structureDefault = Json.createReader(readerDefault).read();
+            JsonStructure structureMerge = Json.createReader(readerMerge).read();
+            try {
+                jsonString = String.valueOf(Json.createMergePatch(structureMerge).apply(structureDefault));
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void mergeDiff() {
+        try (StringReader readerDefault = new StringReader(defaultJsonString);
+             StringReader readerResult = new StringReader(expectedMergeResult);
+        ) {
+            JsonStructure structureDefault = Json.createReader(readerDefault).read();
+            JsonStructure structureResult = Json.createReader(readerResult).read();
+            try {
+                jsonString = String.valueOf(Json.createMergeDiff(structureDefault, structureResult).toJsonValue());
+
+            } catch (Exception e) {
+                jsonString = e.getMessage();
+            }
+        }
+    }
+
+    public void jsonCollector() {
+        JsonArray array = IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> Json.createObjectBuilder()
+                        .add("key" + i, "value" + i)
+                        .build())
+                .collect(JsonCollectors.toJsonArray());
+        jsonString = String.valueOf(array);
     }
 
     public void parseJsonStream() {
@@ -96,4 +270,19 @@ public class JsonController {
         return parsedResult;
     }
 
+    public String getPointerResult() {
+        return pointerResult;
+    }
+
+    public void setPointerResult(String pointerResult) {
+        this.pointerResult = pointerResult;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
 }
